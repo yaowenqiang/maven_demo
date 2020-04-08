9a5e645596c8bd82ce2412e39c3411249b507379
yaowenqiang <yaowenqiang111@163.com>
2020-04-09 04:21:51 +0800

    add logger dependency

--- src/main/java/com/yaowenqinag/App.java
+++ src/main/java/com/yaowenqinag/App.java
@@ -1,4 +1,5 @@
 package com.yaowenqinag;
+import org.slf4j.*;
 
 /**
  * Hello world!
@@ -8,6 +9,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("hello world");
     }
 }
